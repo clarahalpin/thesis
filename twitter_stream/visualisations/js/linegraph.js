c570b1681f86fba31be3a0afb8325d653f0eecc0
clarahalpin <claramhalpin@gmail.com>
2017-07-21 14:48:33 +0100

    changes

--- twitter_stream/visualisations/js/linegraph.js
+++ twitter_stream/visualisations/js/linegraph.js
@@ -7,20 +7,20 @@ $(document).ready(function () {
 	client.ping({
             // ping usually has a 3000ms timeout
             requestTimeout: 1000
-        }, function (error) {
-        	if (error) {
-        		console.trace('elasticsearch cluster is down!');
-        	} else {
-        		console.log("elasticsearch is running");
-        	}
-        });
+    }, function (error) {
+      	if (error) {
+       		console.trace('elasticsearch cluster is down!');
+       	} else {
+       		console.log("elasticsearch is running");
+       	}
+    });
 
 	var query ={
 		"query": {
 			"range" : {
 				"created_at" : {
-					"gte" :"Thu Jun 15 00:00:00 +0000 2017",
-					"lt" : "Fri Jun 16 00:00:00 +0000 2017"
+					"gte" :"Thu Jun 08 00:00:00 +0000 2017",
+					"lt" : "Fri Jun 09 00:00:00 +0000 2017"
 				}
 			}
 		},
@@ -29,12 +29,15 @@ $(document).ready(function () {
 				"date_histogram": {
 					"field": "created_at",
 					"interval": "hour"
-				}}},
-     "size":0
-        
-        
-      }    
-
+				}
+			}
+		},
+		"size":0
+	}    
+$('#sandbox-container .input-group.date').datepicker({
+    format: "dd/mm/yyyy",
+    datesDisabled: ['07/06/2017', '07/21/2017']
+});
 	client.search({
 		index: 'user_live_tweets',
 		type: 'tweet',
@@ -50,57 +53,62 @@ $(document).ready(function () {
 		width = 600,
 		height = 300;
 
-		var parseTime = d3.timeParse("%a %b %d %H:%M:%S %Z %Y");
+		var format = d3.time.format("%a %b %d %H:%M:%S +0000 %Y");
+
+		// set the ranges
+		var x = d3.time.scale().range([0, width]);
+		var y = d3.scale.linear().range([height, 0]);
+
+		var xAxis = d3.svg.axis()
+					.scale(x)
+					.orient("bottom");
+
+		var yAxis = d3.svg.axis()
+					.scale(y)
+					.orient("left");
+
+		// define the line
+		var valueline = d3.svg.line()
+		.x(function(d) { return x(d.key_as_string); })
+		.y(function(d) { return y(d.doc_count); });
+
+		// append the svg obgect to the body of the page
+		// appends a 'group' element to 'svg'
+		// moves the 'group' element to the top left margin
+		var svg = d3.select(".linegraph").append("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform",
+			"translate(" + margin.left + "," + margin.top + ")");
+		
+		days.forEach(function(d) {
+			d.key_as_string = format.parse(d.key_as_string);
+			d.doc_count = +d.doc_count;
+		});
+
+		// Scale the range of the data
+		x.domain(d3.extent(days, function(d) { return d.key_as_string; }));
+		y.domain([0, d3.max(days, function(d) { return d.doc_count; })]);
+
+		// Add the valueline path.
+		svg.append("path")
+		.data([days])
+		.attr("class", "line")
+		.attr("d", valueline);
+
+		svg.append("g")
+        .attr("class", "xaxis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis.tickFormat(d3.time.format("%H:%M")));
+
+		// Add the Y Axis
+    	svg.append("g")
+        .attr("class", "yaxis")
+        .call(yAxis);
 
 
-	// set the ranges
-	var x = d3.scaleTime().range([0, width]);
-	var y = d3.scaleLinear().range([height, 0]);
-
-	// define the line
-	var valueline = d3.line()
-	.x(function(d) { return x(d.key_as_string); })
-	.y(function(d) { return y(d.doc_count); });
-
-	// append the svg obgect to the body of the page
-	// appends a 'group' element to 'svg'
-	// moves the 'group' element to the top left margin
-	var svg = d3.select(".linegraph").append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform",
-		"translate(" + margin.left + "," + margin.top + ")");
-	
-	days.forEach(function(d) {
-		d.key_as_string = parseTime(d.key_as_string);
-		d.doc_count = +d.doc_count;
+	}, function (err) {
+	   	console.trace(err.message);
 	});
-	// Scale the range of the data
-	x.domain(d3.extent(days, function(d) { return d.key_as_string; }));
-	y.domain([0, d3.max(days, function(d) { return d.doc_count; })]);
-
-	  // Add the valueline path.
-	  svg.append("path")
-	  .data([days])
-	  .attr("class", "line")
-	  .attr("d", valueline);
-
-	  // Add the X Axis
-	  svg.append("g")
-	  .attr("transform", "translate(0," + height + ")")
-	  .call(d3.axisBottom(x)
-	  	.tickFormat(d3.timeFormat("%H:%M")));
-
-	  // Add the Y Axis
-	  svg.append("g")
-	  .call(d3.axisLeft(y));
-	            // linechart();
-	        }, function (err) {
-	        	console.trace(err.message);
-	        });
-
-
-
-
 });
--- twitter_stream/visualisations/js/tweet_stack.js
+++ twitter_stream/visualisations/js/tweet_stack.js
@@ -5,17 +5,18 @@ $(document).ready(function () {
 	});
 
 	var query = {
-		"query": {
-			"match_all": {}
-		},
-		"sort": [
-		{
-			"id": {
-				"order": "desc"
-			}
-		}
-		],
-	} 
+  "query": {
+    "match_all": {}
+  },
+  "sort": [
+    {
+      "created_at": {
+        "order": "desc"
+      }
+    }
+  ],
+    "size":10
+}
 	var tweets = [];
 
 
@@ -29,44 +30,20 @@ $(document).ready(function () {
 		//console.log('response:', resp);
 
 		var response = resp.hits.hits;
-
-		
-		var length = 10;
 		
 		for (i = 0; i < response.length; i++) { 
 			var text = response[i]["_source"]["text"];
 		    //console.log(text);
 		    var username = response[i]["_source"]["user"]["screen_name"];
 		    //console.log(username);
-		    //var index = tweets.map(function(o) { return o.key; }).indexOf(text);
-		    //if (index ==-1) {
-		    	tweets[text] = username;
-
-
-		    	// if (tweets.indexOf(item) === -1) {}
-		    	// tweets[text] = username;
-		    	// 	key:   text,
-		    	// 	value: username
-		    	// });
-		    //}
+		    tweets[text] = username;
 		}
 		console.log(tweets);
 
-		// for(var i =0; i<tweets.length; i++) {
-
-			for (var k in tweets){
-    if (tweets.hasOwnProperty(k)) {
-    	if (tweets.indexOf(k) < 0) {
-    	$('#stack').prepend('<li>' +k+ "<br>" +tweets[k]+ '</li>');
-         //alert("Key is " + k + ", value is" + tweets[k]);
-    }}
-}
-			// var items = document.getElementById("stack");
-			// var item = document.createElement("li");
-			// item.innerHTML = tweet_array[i];
-			// items.appendChild(item);
-		// }
-	// }
+		for (var k in tweets){
+    		$('#stack').prepend('<li>' +k+ "<br>" +tweets[k]+ '</li>');
+    		//console.log(k);
+		}
 
 }, function (err) {
 	console.trace(err.message);
