573423932718798989ea70be5a13894a11ea1f1b
Clara <clara@baconbox.computing.dcu.ie>
2017-07-26 16:29:59 +0100

    script and csv to stream users past tweets

--- twitter_stream/user_past.py
+++ twitter_stream/user_past.py
@@ -15,4 +15,31 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-user_ids = [767165447351463937,767165475151306752,]
+user_ids = [767165447351463937, 767165475151306752, 832986425788289025, 767153591421370368, 767149776362151936]
+
+def get_tweets():
+    try:
+        allTweets = []
+
+        for u_id in user_ids:
+            tweets = tweepy.Cursor(api.user_timeline, user_id = u_id).items()
+            allTweets.extend(tweets)
+
+    
+            outtweets = [[tweet.id_str, tweet.created_at, tweet.user.id] for tweet in allTweets]
+
+            with open('user_tweets.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerow(["id","created_at","user_id"])
+                writer.writerows(outtweets)
+
+                f.close()
+    except Exception as e:
+        print ("Error", e )
+
+
+
+if __name__ == '__main__':
+    
+        get_tweets()
+    
